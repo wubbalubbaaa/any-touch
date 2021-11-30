fb21272eab4e32ae0dcef5b127ad3491997d1d81
wubbalubbaaa <543668641@qq.com>
2021-12-01 02:36:53 +0800

    make some dif

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,6 @@ module.exports = {
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
     moduleNameMapper: {
-        '^@any-touch/(.*?)$': '<rootDir>/packages/$1/src',
         '^@testUtils$': '<rootDir>/testUtils/index.ts',
         '^any-touch$': '<rootDir>/packages/any-touch/src',
         '^any-event$': '<rootDir>/packages/any-event/src'
